df520f56407075aa865c28927706d880ad95ccdc
M47Z <38143854+IamM47Z@users.noreply.github.com>
2021-06-29 20:19:13 +0100

    Update FakeDeafen.plugin.js

--- FakeDeafen.plugin.js
+++ FakeDeafen.plugin.js
@@ -1,11 +1,34 @@
 /**
     * @name FakeDeafen
+    * @author M47Z
     * @description This plugin allows you to fake deafen/mute in voice chat.
-    * @version 0.0.1
+    * @version 1.0.0
+    * @source https://github.com/IamM47Z/BetterDiscordPlugins/blob/master/FakeDeafen.plugin.js
+    * @updateUrl https://raw.githubusercontent.com/IamM47Z/BetterDiscordPlugins/master/FakeDeafen.plugin.js
     */
-
-const text = new TextDecoder("utf-8");
-
+/*@cc_on
+@if (@_jscript)
+    
+    // Offer to self-install for clueless users that try to run this directly.
+    var shell = WScript.CreateObject("WScript.Shell");
+    var fs = new ActiveXObject("Scripting.FileSystemObject");
+    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+    var pathSelf = WScript.ScriptFullName;
+    // Put the user at ease by addressing them in the first person
+    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+    } else if (!fs.FolderExists(pathPlugins)) {
+        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+        // Show the user where to put plugins in the future
+        shell.Exec("explorer " + pathPlugins);
+        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+    }
+    WScript.Quit();
+@else@*/
+/* Generated Code */
 module.exports = (_ => {
     const config = {
         "info": {
