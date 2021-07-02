424bb57cc96353233e1f7a5b3d7dcabe676c14d7
M47Z <38143854+IamM47Z@users.noreply.github.com>
2021-07-02 01:17:26 +0100

    radical switch to the main api
    we are no longer using DevilBro Library :D

--- QuickDeleteMessages.plugin.js
+++ QuickDeleteMessages.plugin.js
@@ -1,131 +1,125 @@
 /**
-    * @name QuickDeleteMessages
-    * @author M47Z
-    * @description This plugin allows you to delete messages by pressing delete button.
-    * @version 1.0.0
-    * @source https://github.com/IamM47Z/BetterDiscordPlugins/blob/master/QuickDeleteMessages.plugin.js
-    * @updateUrl https://gitcdn.link/repo/IamM47Z/BetterDiscordPlugins/master/QuickDeleteMessages.plugin.js
-    */
+ * @name QuickDeleteMessages
+ * @invite undefined
+ * @authorLink undefined
+ * @donate undefined
+ * @patreon undefined
+ * @website https://github.com/IamM47Z/BetterDiscordPlugins/blob/master/QuickDeleteMessages.plugin.js
+ * @source https://raw.githubusercontent.com/IamM47Z/BetterDiscordPlugins/master/QuickDeleteMessages.plugin.js
+ */
+/*@cc_on
+@if (@_jscript)
+	
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+		// Show the user where to put plugins in the future
+		shell.Exec("explorer " + pathPlugins);
+		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+	}
+	WScript.Quit();
 
-module.exports = (_ => {
-    const config = {
-        "info": {
-            "name": "QuickDeleteMessages",
-            "author": "M47Z",
-            "version": "1.0.0",
-            "description": "This plugin allows you to delete messages by pressing delete button."
-        }
-    };
+@else@*/
 
-    return (window.Lightcord || window.LightCord) ? class {
-        getName() { return config.info.name; }
-        getAuthor() { return config.info.author; }
-        getVersion() { return config.info.version; }
-        getDescription() { return "Do not use LightCord!"; }
-        load() { BdApi.alert("Attention!", "By using LightCord you are risking your Discord Account, due to using a 3rd Party Client. Switch to an official Discord Client (https://discord.com/) with the proper BD Injection (https://betterdiscord.app/)"); }
-        start() { }
-        stop() { }
-    } : !window.BDFDB_Global || (!window.BDFDB_Global.loaded && !window.BDFDB_Global.started) ? class {
-        getName() { return config.info.name; }
-        getAuthor() { return config.info.author; }
-        getVersion() { return config.info.version; }
-        getDescription() { return `The Library Plugin needed for ${config.info.name} is missing. Open the Plugin Settings to download it. \n\n${config.info.description}`; }
+module.exports = (() => {
+    const config = {"info":{"name":"QuickDeleteMessages","authors":[{"name":"M47Z","discord_id":"398917073444798466","github_username":"IamM47Z"}],"version":"1.0.0","description":"This plugin allows you to delete messages by pressing delete button.","github":"https://github.com/IamM47Z/BetterDiscordPlugins/blob/master/QuickDeleteMessages.plugin.js","github_raw":"https://raw.githubusercontent.com/IamM47Z/BetterDiscordPlugins/master/QuickDeleteMessages.plugin.js"},"changelog":[{"title":"New Stuff","type":"improved","items":["BDFDB Api Removed"]},{"title":"On-going","type":"progress","items":["Avoid Discord Rate-Limit (possible adjustable cooldown)"]}],"defaultConfig":[{"type":"switch","id":"confirmDelete","name":"Confirm Delete","note":"Show Confirm Popup when attempting to Delete Messages","value":true}],"main":"index.js"};
 
-        downloadLibrary() {
-            require("request").get("https://mwittrien.github.io/BetterDiscordAddons/Library/0BDFDB.plugin.js", (e, r, b) => {
-                if (!e && b && r.statusCode == 200) require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0BDFDB.plugin.js"), b, _ => BdApi.showToast("Finished downloading BDFDB Library", { type: "success" }));
-                else BdApi.alert("Error", "Could not download BDFDB Library Plugin. Try again later or download it manually from GitHub: https://mwittrien.github.io/downloader/?library");
+    return !global.ZeresPluginLibrary ? class {
+        constructor() {this._config = config;}
+        getName() {return config.info.name;}
+        getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
+        getDescription() {return config.info.description;}
+        getVersion() {return config.info.version;}
+        load() {
+            BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
+                confirmText: "Download Now",
+                cancelText: "Cancel",
+                onConfirm: () => {
+                    require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                        if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+                    });
+                }
             });
         }
+        start() {}
+        stop() {}
+    } : (([Plugin, Api]) => {
+        const plugin = (Plugin, Api) => {
+    const { DiscordAPI, DiscordModules, DOMTools, WebpackModules } = Api;
 
-        load() {
-            if (!window.BDFDB_Global || !Array.isArray(window.BDFDB_Global.pluginQueue)) window.BDFDB_Global = Object.assign({}, window.BDFDB_Global, { pluginQueue: [] });
-            if (!window.BDFDB_Global.downloadModal) {
-                window.BDFDB_Global.downloadModal = true;
-                BdApi.showConfirmationModal("Library Missing", `The Library Plugin needed for ${config.info.name} is missing. Please click "Download Now" to install it.`, {
-                    confirmText: "Download Now",
-                    cancelText: "Cancel",
-                    onCancel: _ => { delete window.BDFDB_Global.downloadModal; },
-                    onConfirm: _ => {
-                        delete window.BDFDB_Global.downloadModal;
-                        this.downloadLibrary();
-                    }
-                });
-            }
-            if (!window.BDFDB_Global.pluginQueue.includes(config.info.name)) window.BDFDB_Global.pluginQueue.push(config.info.name);
+    let _this;
+    let MessageUtils;
+    let mouseX, mouseY;
+
+    return class QuickDeleteMessages extends Plugin {
+        onMouseMove(event) {
+            mouseX = event.clientX;
+            mouseY = event.clientY;
         }
-        start() { this.load(); }
-        stop() { }
+
+        onKeyDown(event) {
+            if (event.keyCode != 46 || (process.platform == "darwin" && event.keyCode != 27))
+                return;
+
+            let targetElem = document.elementFromPoint(mouseX, mouseY);
+            if (!targetElem)
+                return;
+
+            let messageElem = targetElem.className.includes("message-2qnXI6") ? targetElem : DOMTools.parents(targetElem, ".message-2qnXI6")[0];
+            if (!messageElem)
+                return;
+
+            let messageInfo = BdApi.getInternalInstance(messageElem);
+            if (!messageInfo)
+                return;
+
+            messageInfo = messageInfo.memoizedProps.children[3].props;
+
+            if (!DiscordModules.DiscordConstants.MessageTypesDeletable[messageInfo.message.type] || !messageElem.querySelector(".messageContent-2qWWxC"))
+                return;
+
+            if (messageInfo.message.author.id != DiscordAPI.currentUser.id && !DiscordModules.Permissions.can("MANAGE_MESSAGES", DiscordAPI.currentUser.id, messageInfo.channel.id))
+                return;
+
+            if (!event.shiftKey && _this.settings["confirmDelete"])
+                MessageUtils.confirmDelete(messageInfo.channel, messageInfo.message, false);
+            else
+                DiscordModules.MessageActions.deleteMessage(messageInfo.channel.id, messageInfo.message.id, messageInfo.message.state != DiscordModules.DiscordConstants.MessageStates.SENT);
+        }
+
+        onStart() {
+            _this = this;
+
+            MessageUtils = WebpackModules.getByProps("confirmDelete");
+
+            document.addEventListener("keydown", this.onKeyDown);
+            document.addEventListener("mousemove", this.onMouseMove);
+        }
+
+        onStop() {
+            document.removeEventListener("keydown", this.onKeyDown);
+            document.removeEventListener("mousemove", this.onMouseMove);
+        }
+
         getSettingsPanel() {
-            let template = document.createElement("template");
-            template.innerHTML = `<div style="color: var(--header-primary); font-size: 16px; font-weight: 300; white-space: pre; line-height: 22px;">The Library Plugin needed for ${config.info.name} is missing.\nPlease click <a style="font-weight: 500;">Download Now</a> to install it.</div>`;
-            template.content.firstElementChild.querySelector("a").addEventListener("click", this.downloadLibrary);
-            return template.content.firstElementChild;
+            const settingsPanel = this.buildSettingsPanel();
+
+            return settingsPanel.getElement();
         }
-    } : (([Plugin, BDFDB]) => {
-        return class QuickDeleteMessages extends Plugin {
-            onLoad() {
-                this.defaults = {
-                    general: {
-                        confirmDelete: { value: true, description: "Confirm Delete" }
-                    }
-                };
-            }
-
-            onStart() {
-                let MessageUtils = BDFDB.ModuleUtils.findByProperties("confirmDelete");
-
-                BDFDB.ListenerUtils.add(this, document, "keydown", event => {
-                    if (event.keyCode != BDFDB.LibraryModules.KeyEvents("delete") ||
-                        (process.platform == "darwin" && event.keyCode != BDFDB.LibraryModules.KeyEvents("escape")))
-                        return;
-
-                    let messageElem = BDFDB.DOMUtils.getParent(BDFDB.dotCN.message, document.activeElement);
-                    if (!messageElem)
-                        return;
-
-                    let messageInfo = BdApi.getInternalInstance(messageElem);
-                    if (!messageInfo)
-                        return;
-
-                    messageInfo = messageInfo.memoizedProps.children[3].props;
-
-                    if (!BDFDB.DiscordConstants.MessageTypesDeletable[messageInfo.message.type] || !messageElem.querySelector(".messageContent-2qWWxC"))
-                        return;
-
-                    if (messageInfo.message.author.id != BDFDB.UserUtils.me.id && !BDFDB.UserUtils.can("MANAGE_MESSAGES", BDFDB.UserUtils.me.id, messageInfo.channel.id))
-                        return;
-
-                    if (this.settings.general["confirmDelete"] || BDFDB.ListenerUtils.isPressed(BDFDB.LibraryModules.KeyEvents("shift")))
-                        MessageUtils.confirmDelete(messageInfo.channel, messageInfo.message, false);
-                    else
-                        BDFDB.LibraryModules.MessageUtils.deleteMessage(messageInfo.channel.id, messageInfo.message.id, messageInfo.message.state != BDFDB.DiscordConstants.MessageStates.SENT);
-                });
-            }
-
-            onStop() {
-                BDFDB.PatchUtils.forceAllUpdates(this);
-            }
-
-            getSettingsPanel(collapseStates = {}) {
-                let settingsPanel;
-                return settingsPanel = BDFDB.PluginUtils.createSettingsPanel(this, {
-                    collapseStates: collapseStates,
-                    children: _ => {
-                        let settingsItems = [];
-
-                        for (let key in this.defaults.general) settingsItems.push(BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.SettingsSaveItem, {
-                            type: "Switch",
-                            plugin: this,
-                            keys: ["general", key],
-                            label: this.defaults.general[key].description,
-                            value: this.settings.general[key]
-                        }));
-
-                        return settingsItems;
-                    }
-                });
-            }
-        };
-    })(window.BDFDB_Global.PluginUtils.buildPlugin(config));
+    };
+};
+        return plugin(Plugin, Api);
+    })(global.ZeresPluginLibrary.buildPlugin(config));
 })();
+/*@end@*/
